0a6ec3d89367a4e10a2efd7079efe28ef6ee6367
Nguyen Van Anh <abcgohan123mam@gmail.com>
2025-03-19 03:01:43 +0700

    lần 7

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(cors({
     origin: ['https://checkscam.net.vn'],
     methods: ['GET', 'POST', 'OPTIONS'],  // Cho phép phương thức OPTIONS
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Cho phép các cookie được gửi đến server
 }));
 
 // Đảm bảo backend của bạn xử lý preflight requests
